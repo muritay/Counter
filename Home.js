bb138e3bf2e900cc15cbfb58270a7490f1edd6b8
Babajide Yusuf <47525113+muritay@users.noreply.github.com>
2025-04-27 00:36:22 +0100

    Update Home.js

--- Home.js
+++ Home.js
@@ -6,12 +6,12 @@ function Home () {
     
 
     const increment = () => {
-        setCount (count +1);
+        setCount ((prevCount) => prevCount +1)
     };
 
     const decrement =() => {
         if ( count > 0) {
-            setCount (count -1);
+           setCount ((prevCount) => prevCount -1)
         }
     };
 
